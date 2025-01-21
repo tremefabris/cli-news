e84ff95fc6e18283dc17005038dce01756ea5247
tremefabris <fabris.trab@gmail.com>
2025-01-21 20:35:34 -0300

    add better logging system

---
+++ src/log.py
@@ -0,0 +1,25 @@
+from .color import Color as C
+
+
+# TODO: Change `level` to enum
+class Log():
+
+    OK = 0
+    WARNING = 1
+    ERROR = 2
+
+    def __init__(self, *messages, level=None):
+        if level not in [Log.OK, Log.WARNING, Log.ERROR]:
+            raise ValueError("`level` should be one of `Log.OK`, `Log.WARNING`, `Log.ERROR`")
+        
+        level_color = {
+            Log.OK: C.green("OK"),
+            Log.WARNING: C.yellow("WARNING"),
+            Log.ERROR: C.red("ERROR"),
+        }
+
+        for message in messages:
+            print(f" :: {level_color[level]} :: {message}...")
+
+        if level == Log.ERROR:
+            raise RuntimeError("please, check messages above")
